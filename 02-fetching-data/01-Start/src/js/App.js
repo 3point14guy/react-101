3b01bc4024c80db9a874a9edef3e26be66868b4b
3poitn14guy <nicholas.piaskoski@gmail.com>
2018-05-20 20:51:53 -0400

    add code along from module 2

--- 02-fetching-data/01-Start/src/js/App.js
+++ 02-fetching-data/01-Start/src/js/App.js
@@ -3,24 +3,92 @@ import image from '../images/cloud-upload-download-data-transfer.svg';
 import Collapsible from './Collapsible';
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      contacts: []
+    }
+  }
+
+  componentWillMount() {
+    localStorage.getItem('contacts') && this.setState({
+      contacts: JSON.parse(localStorage.getItem('contacts')),
+      isLoading: false
+    })
+  }
+  // componentDidMount is the react component for fetching info from 3rd party APIs
+  componentDidMount(){
+
+    const date = localStorage.getItem('contactsDate');
+    const contactsDate = date && new Date(parseInt(date));
+    const now = new Date();
+
+    const dataAge = Math.round((now - contactsDate) / (1000 * 60));
+    const tooOld = dataAge >= 15;
+
+
+    if(tooOld){
+      this.fetchData();
+    } else {
+      console.log(`Using data from localStorge that is ${dataAge} minutes old.`);
+  }
+
+  }
+
+  fetchData(){
+
+    this.setState({
+      isLoading: true,
+      contacts: []
+    })
+    fetch('https://randomuser.me/api/?results=50&nat=us,dk,fr,gb')
+    .then(response => response.json())
+    .then(parsedJSON => parsedJSON.results.map(user => (
+      {
+        name: `${user.name.first} ${user.name.last}`,
+        username: `${user.login.username}`,
+        email: `${user.email}`,
+        location: `${user.location.street}, ${user.location.city}`
+    }
+  )))
+    .then(contacts => this.setState ({
+      contacts,
+      isLoading: false
+    }))
+    .catch(error => console.log('parsing failed', error))
+  }
+
+componentWillUpdate(nextProps, nextState) {
+  localStorage.setItem('contacts', JSON.stringify(nextState.contacts));
+  localStorage.setItem('contactsDate', Date.now());
+}
+
+
     render() {
+      const {isLoading, contacts} = this.state;
         return (
             <div>
                 <header>
                     <img src={image} />
-                    <h1>Fetching Data <button className="btn btn-sm btn-danger">Fetch now</button></h1>
+                    <h1>Fetching Data <button className="btn btn-sm btn-danger" onClick={(e) => { this.fetchData();
+                    }}>Fetch now</button></h1>
                 </header>
-                <div className="content">
+                <div className={`content ${isLoading ? 'is-loading' : ''}`}>
                     <div className="panel-group">
-                        <Collapsible title="Tracy Palmer">
-                            <p>tracey.palmer@example.com<br />3280 manchester road, ely</p>
-                        </Collapsible>
-                        <Collapsible title="Jade Dupuis">
-                            <p>jade.dupuis@example.com<br />1803 rue pasteur, nanterre</p>
-                        </Collapsible>
-                        <Collapsible title="Alfred Olsen">
-                            <p>alfred.olsen@example.com<br />6598 fjordparken, hirtsals</p>
-                        </Collapsible>
+                      {
+                        !isLoading && contacts.length > 0 ? contacts.map(contact => {
+                          const {username, name, email, location} = contact;
+                          return <Collapsible key={username} title={name}>
+                            <p>{email}<br />{location}</p>
+                            </Collapsible>
+                        }) : null
+                      }
+
+                    </div>
+                    <div className="loader">
+                      <div className="icon"></div>
                     </div>
                 </div>
             </div>
